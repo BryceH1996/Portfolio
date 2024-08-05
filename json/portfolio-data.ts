3d5ed353275321a408b652208377b8177759ba2d
Bryce <brycehardie01@gmail.com>
2024-08-05 22:15:40 +0200

    Built out json to handle portfolio data and setup temp display

---
+++ json/portfolio-data.ts
@@ -0,0 +1,16 @@
+export const portfolioData = [
+  {
+    project: "onTap",
+    techStack: ['Vue.JS', 'C# backend', 'Vue Storefront'],
+    favourited: true,
+    shortDescription: 'During 2020-2021 onTap company wanted to increase their digital online presence and approached the agency I was working with to assist them. \n\n At the time we were experimenting with Vue Storefront with intergrations into the companies "Nimble" product, a backend management software written in C#',
+    mainImage: '/a.png'
+  },
+  {
+    project: "onTap",
+    techStack: ['Vue.JS', 'C# backend', 'Vue Storefront'],
+    favourited: true,
+    shortDescription: 'During 2020-2021 onTap company wanted to increase their digital online presence and approached the agency I was working with to assist them. \n\n At the time we were experimenting with Vue Storefront with intergrations into the companies "Nimble" product, a backend management software written in C#',
+    mainImage: '/a.png'
+  }
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,12 @@
 import CardLayout from "@/components/card-layout/card-layout";
 import PrimaryButton from "../components/button/primary-button";
 import Image from "next/image";
-import { delay, motion } from "framer-motion"
+import { motion } from "framer-motion"
 import { Autoplay, Controller, Navigation, Pagination } from 'swiper/modules';
 import { type Swiper as SwiperTypes } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { useState } from "react";
+import { portfolioData } from "../../json/portfolio-data";
 
 export default function Home() {
 
@@ -92,72 +93,59 @@ export default function Home() {
         </div>
       </CardLayout>
       <CardLayout minHeight="min-h-full" primary="bg-primary" secondary="bg-white" layer="z-10" margining="-mt-4">
-        <div className="container mx-auto flex w-full flex-row xl:pt-6 xl:pb-12 pt-4 pb-8">
-          <div className="lg:w-6/12 xl:pr-16 -top-24 relative -mb-24">
-            <Swiper 
-              modules={[Controller]}
-              onSwiper={setFirstSwiper}
-              controller={{ control: secondSwiper }}>
-              <SwiperSlide>
-                <Image
-                  className="relative"
-                  src="a.png"
-                  alt="Portrait photo"
-                  width={0}
-                  height={0}
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </SwiperSlide>
-              <SwiperSlide>
-                <Image
-                  className="relative"
-                  src="a.png"
-                  alt="Portrait photo"
-                  width={0}
-                  height={0}
-                  style={{ width: '100%', height: 'auto' }}
-                />
-              </SwiperSlide>
-            </Swiper>
-          </div>
-          <div className="lg:w-6/12 xl:pl-16">
-          <h2 className="xl:text-4xl text-3xl font-bold text-secondary">Favourite Projects</h2>
-            <Swiper         
-              modules={[Controller, Pagination, Navigation, Autoplay]}
-              onSwiper={setSecondSwiper}
-              controller={{ control: firstSwiper }}
-              loop={true}
-              autoplay={{delay: 10000}}
-              navigation
-              pagination={{type: 'fraction',
-                renderFraction: (currentClass, totalClass) => {
-                  return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>'
-                  }
-              }}>
-              <SwiperSlide>
-                <div className="text-secondary">
-                  <h3 className="font-bold text-md mt-4 mb-4">Portfolio Title</h3>
-                  <p className="text-sm">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ipsum urna, tristique at porta in, scelerisque quis nisl. 
-                    <br /><br />
-                    Curabitur enim odio, ultrices sed iaculis eu, rutrum et nunc. Phasellus in sem quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer convallis purus et tellus imperdiet interdum. 
-                  </p>
-                </div>
-              </SwiperSlide>
-              <SwiperSlide>
-              <div className="text-secondary">
-                <h3 className="font-bold text-md mt-4 mb-4">Portfolio Title</h3>
-                <p className="text-sm">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam ipsum urna, tristique at porta in, scelerisque quis nisl. 
-                  <br /><br />
-                  Curabitur enim odio, ultrices sed iaculis eu, rutrum et nunc. Phasellus in sem quam. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Integer convallis purus et tellus imperdiet interdum. 
-                </p>
+            <div className="container mx-auto flex w-full flex-row xl:pt-6 xl:pb-12 pt-4 pb-8">
+              <div className="lg:w-6/12 xl:pr-16 -top-24 relative -mb-24">
+                <Swiper 
+                  modules={[Controller]}
+                  onSwiper={setFirstSwiper}
+                  controller={{ control: secondSwiper }}>
+                  {portfolioData.map((data, key) => {
+                    return (
+                      <SwiperSlide key={key}>
+                        <Image
+                          className="relative"
+                          src={data.mainImage}
+                          alt="Portrait photo"
+                          width={0}
+                          height={0}
+                          style={{ width: '100%', height: 'auto' }}
+                        />
+                      </SwiperSlide>
+                    )
+                  })}
+                </Swiper>
               </div>
-              </SwiperSlide>
-              <PrimaryButton className="inline-block mt-6" text="View portfolio" url="/portfolio"/>
-            </Swiper>
-          </div>
-        </div>
+              <div className="lg:w-6/12 xl:pl-16">
+              <h2 className="xl:text-4xl text-3xl font-bold text-secondary">Favourite Projects</h2>
+                <Swiper         
+                  modules={[Controller, Pagination, Navigation, Autoplay]}
+                  onSwiper={setSecondSwiper}
+                  controller={{ control: firstSwiper }}
+                  loop={true}
+                  autoplay={{delay: 10000}}
+                  navigation
+                  pagination={{type: 'fraction',
+                    renderFraction: (currentClass, totalClass) => {
+                      return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>'
+                      }
+                  }}>
+                  {portfolioData.map((data, key) => {
+                    return (
+                      <SwiperSlide key={key}>
+                      <div className="text-secondary">
+                        <h3 className="font-bold text-md mt-4 mb-4">Project: {data.project}</h3>
+                        <p className="text-sm whitespace-pre-line">
+                          {data.shortDescription}
+                        </p>
+                      </div>
+                      </SwiperSlide>
+                    )
+                  })}
+                  <PrimaryButton className="inline-block mt-6" text="View portfolio" url="/portfolio"/>
+                </Swiper>
+              </div>
+            </div>
+
       </CardLayout>
     </main>
   );
